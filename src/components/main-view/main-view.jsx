6bfeabe577accf0c8c81d998a8ba5be212183dc2
Polodolo4 <86023627+Polodolo4@users.noreply.github.com>
2022-06-01 18:38:09 -0400

    update main view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
 
  constructor(){
     super();
