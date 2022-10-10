8a0498e5de7598b2a85c9e7d2239df970eb13052
Polodolo4 <86023627+Polodolo4@users.noreply.github.com>
2022-10-10 13:57:01 -0400

    Add/Remove Movie Refactor

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,16 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import { SET_FILTER, SET_MOVIES, SET_USER } from '../actions/actions';
+import {
+  SET_FILTER,
+  SET_MOVIES,
+  SET_USER,
+  SET_FAVORITE,
+} from "../actions/actions";
 
-function visibilityFilter(state = '', action) {
+function visibilityFilter(state = "", action) {
   switch (action.type) {
     case SET_FILTER:
-      console.log('SET_FILTER reducer reached')
+      console.log("SET_FILTER reducer reached");
       return action.value;
     default:
       return state;
@@ -15,17 +20,27 @@ function visibilityFilter(state = '', action) {
 function movies(state = [], action) {
   switch (action.type) {
     case SET_MOVIES:
-      console.log('SET_MOVIES reducer reached')
+      console.log("SET_MOVIES reducer reached");
       return action.value;
     default:
       return state;
   }
 }
 
-function user(state = '', action) {
+function user(state = "", action) {
   switch (action.type) {
     case SET_USER:
-      console.log('SET_USER reducer reached')
+      console.log("SET_USER reducer reached");
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function favorite(state = [], action) {
+  switch (action.type) {
+    case SET_FAVORITE:
+      console.log("SET_FAVORITE reducer reached");
       return action.value;
     default:
       return state;
@@ -35,7 +50,8 @@ function user(state = '', action) {
 const moviesApp = combineReducers({
   visibilityFilter,
   movies,
-  user
+  user,
+  favorite,
 });
 
 export default moviesApp;
